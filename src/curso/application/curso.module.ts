5160516830f9a336d8e005c7b47e33111e34985e
TalissaMoura <talissa.ayres@gmail.com>
2024-08-26 21:59:36 -0400

    update imports for curso module

--- src/curso/application/curso.module.ts
+++ src/curso/application/curso.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CursoService } from './curso.service';
-import { CursoController } from '../curso.controller';
-import { AlunoModule } from 'src/aluno/application/aluno.module';
-import { AlunoInfrastructureModule } from 'src/aluno/infrastructure/aluno-infrastructure.module';
+import { CursoController } from '../presenters/curso.controller';
+import { AlunoModule } from '../../aluno/application/aluno.module';
+import { AlunoInfrastructureModule } from '../../aluno/infrastructure/aluno-infrastructure.module';
+import { CursoFactory } from '../domain/factory/curso.factory';
+import { InMemoryCursoRepository } from '../infrastructure/persistence/in-memory/repositories/curso.repository';
+import { AlunoService } from '../../aluno/application/aluno.service';
+
 @Module({
   imports: [AlunoModule.comInfraestrutura(AlunoInfrastructureModule.use("in-memory"))],
   controllers: [CursoController],
-  providers: [CursoService],
+  providers: [CursoService,CursoFactory,InMemoryCursoRepository],
 })
 export class CursoModule {}
